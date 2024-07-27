fea3cab8103cacfe9a07a2a6b6ee8546ea81b5ba
Rohan <rohanvarma911@gmail.com>
2024-07-27 23:09:33 +0530

    Fetch & Display Projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from './components/Clients';
+import AddClientModal from './components/AddClientModal';
+
+
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -14,6 +36,7 @@ function App() {
     <ApolloProvider client={client}>
       <Header />
       <div className="container">
+        <AddClientModal />
         <Clients />
       </div>
     </ApolloProvider>
